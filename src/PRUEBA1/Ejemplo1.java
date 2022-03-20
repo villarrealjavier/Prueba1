53a53fac710ec692fbf6e48be03ee4049d6f1397
villarrealjavier <villarrealhinojojavier@gmail.com>
2022-03-20 17:25:39 +0100

    Actualizacion ejemplo

---
+++ src/PRUEBA1/Ejemplo1.java
@@ -0,0 +1,10 @@
+package PRUEBA1;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola esto es una prueba");
+	}
+
+}
